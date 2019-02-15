f7fc4eca79d50570e6250e32906ae175eee68203
jamesbutler01 <jamesbutler01@hotmail.co.uk>
2019-02-15 16:44:28 +0000

    Add RegressionAnalysis comments

--- ExampleAnalysis_Regression.py
+++ ExampleAnalysis_Regression.py
@@ -1,6 +1,5 @@
 import Details as D
 import numpy as np
-import multiprocessing.managers
 import ShiftTrials
 import Utils
 import Maths
@@ -8,38 +7,51 @@ import Plot
 Shifter = ShiftTrials.Shift(2)
 
 
-class MyManager(multiprocessing.managers.BaseManager):
-    pass
-
-
-MyManager.register('np_zeros', np.zeros, multiprocessing.managers.ArrayProxy)
-
-
-def analysecell(counter, out_betas, data, cell):
+def analysecell(counter, output_betas, data, cell):
+    # Print current progress in console
     Utils.updatecounts(counter, cell, data.n)
 
-    td = data.behavdata[cell]
+    # Load trial data
+    trialdata = data.behavdata[cell]
 
-    masks = (D.get_A_AR_trials(td), D.get_A_AC_trials(td), D.get_A_BR_trials(td), D.get_A_BC_trials(td))
+    # Lets do a regression for the reward values they are given
+    x = trialdata.rewgiven
 
-    x = td.previousreward
+    # Let's compare rare versus common trials
+    common_trials = trialdata.transition == 1
+    rare_trials = trialdata.transition == 2
 
+    # Put the different conditions into a single list
+    conds = (common_trials, rare_trials)
+
+    # Loop through each trial epoch that we are interested in
     for i_epoch, epoch in enumerate(D.epochs):
+
+        # Load the y (cell firing rate) for that epoch
         y = data.generatenormalisedepoch(cell, epoch)
 
-        for i_mask, mask in enumerate(masks):
-            out_betas[0, i_mask, i_epoch, cell] = Maths.regression(x[mask], y[mask])
+        # Loop through each condition
+        for i_cond, cond in enumerate(conds):
 
+            # Get data from just trials for this condition
+            x_cond = x[cond]
+            y_cond = y[cond]
+
+            # Do regression on x and y data for this condition and get the beta value
+            beta_val = Maths.regression(x_cond, y_cond)
+
+            # Store the result in the output array
+            output_betas[0, i_cond, i_epoch, cell] = beta_val
 
 
 if __name__ == "__main__":
     import ManagerAnalysis
 
-    maintitle = 't0 betas by t1 outcome'
+    maintitle = 'Average betas for reward on common and rare trials'
     ytitles = 'Mean Betas'
-    savefolder = 'reg/AAR/t1'
-    trace_names = ('A(x)_AR', 'A(x)_AC', 'A(x)_BR', 'A(x)_BC')
+    savefolder = 'reg'
+    trace_names = ('Common trials', 'Rare trials')
     num_conds = len(trace_names)
     num_rows = 1
     plotfunc = Plot.GeneralPlot
-    ManagerAnalysis.Run(analysecell, False, num_conds, num_rows, maintitle, ytitles, savefolder, trace_names, plotfunc)
+    ManagerAnalysis.Run(analysecell, True, num_conds, num_rows, maintitle, ytitles, savefolder, trace_names, plotfunc)
